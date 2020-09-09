7b2c0f9550ea790358f5c8dc752aac13b40cb6e8
Milla <milla.kortelainen@helsinki.fi>
2020-09-09 13:58:26 +0300

    fixing github actions

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { login } from '../reducers/userReducer'
 
 const Login = () => {
