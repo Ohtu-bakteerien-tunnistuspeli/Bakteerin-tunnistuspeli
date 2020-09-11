452e900946f76039fa13219216f132f32816d602
millakortelainen <milla.kortelainen@helsinki.fi>
2020-09-11 11:41:07 +0300

    test for bacteria listing to backend

--- backend/tests/bacterium_api.test.js
+++ backend/tests/bacterium_api.test.js
@@ -4,6 +4,23 @@ const app = require('../app')
 
 const api = supertest(app)
 
+//const Bacterium = require('../models/bacterium')
+/*
+const initialBacteria = [{
+    name: 'koli'
+},
+{
+    name: 'tetanus'
+}]
+*/
+beforeEach(async () => {
+    //await Bacterium.deleteMany({})
+    //let bacteriumObj = new Bacterium(initialBacteria[0])
+    //await bacteriumObj.save()
+    //bacteriumObj = new Bacterium(initialBacteria[1])
+    //await bacteriumObj.save()
+})
+
 test('bacteria are returned as json', async () => {
     const user = await api
         .post('/api/user/login')
@@ -13,9 +30,47 @@ test('bacteria are returned as json', async () => {
         })
         .expect(200)
 
-    const result = await api
+    await api
         .get('/api/bacteria')
         .set('Authorization', `bearer ${user.body.token}`)
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
+
+test('user can add a bacterium', async () => {
+
+    const user = await api
+        .post('/api/user/login')
+        .send({
+            username: 'username',
+            password: 'password'
+        })
+
+    const res = await api
+        .get('/api/bacteria')
+        .set('Authorization', `bearer ${user.body.token}`)
+
+    const initialLength = res.body.length
+
+    const newBacterium = {
+        name: 'testing bacterium'
+    }
+
+    await api
+        .post('/api/bacteria')
+        .set('Authorization', `bearer ${user.body.token}`)
+        .send(newBacterium)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const resAfterAdding = await api
+        .get('/api/bacteria')
+        .set('Authorization', `bearer ${user.body.token}`)
+
+    expect(resAfterAdding.body).toHaveLength(initialLength+1)
+})
+/*
+afterAll(() => {
+    mongoose.connection.close()
+  })
+  */
