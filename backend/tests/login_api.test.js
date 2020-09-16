e1e440f1aea1fee8a35b747b60cab4b5034e778a
Jeeses313 <jesse.vilkman@hotmail.fi>
2020-09-16 14:55:07 +0300

    Test actions testing

--- backend/tests/login_api.test.js
+++ backend/tests/login_api.test.js
@@ -1,16 +1,27 @@
-
-//const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 // const { TestScheduler } = require('jest')
 
 const api = supertest(app)
+const User = require('../models/user')
+
+beforeEach(async () => {
+    await User.remove({})
+    const adminPassword = await bcrypt.hash('admin', 10)
+    const userPassword = await bcrypt.hash('password', 10)
+    const admin = new User({ username: 'adminNew', passwordHash: adminPassword, admin: true })
+    const user = new User({ username: 'usernameNew', passwordHash: userPassword, admin: false })
+    await admin.save()
+    await user.save()
+})
 
 test('login successfull', async () => {
     const user = await api //eslint-disable-line
         .post('/api/user/login')
         .send({
-            username: 'username',
+            username: 'usernameNew',
             password: 'password'
         })
         .expect(200)
@@ -27,8 +38,6 @@ test('failed login', async () => {
         .expect(400)
 })
 
-//afterAll(() => {
-//  mongoose.connection.close()
-//})
-
-// "error": "Invalid username or password"
+afterAll(() => {
+    mongoose.connection.close()
+})
