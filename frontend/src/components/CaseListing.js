b6b020203f10e57822a4ac64a0c529294a6e8350
fannif <fanni.franssila@gmail.com>
2020-09-23 18:08:29 +0300

    fix lint errors

--- frontend/src/components/CaseListing.js
+++ frontend/src/components/CaseListing.js
@@ -9,11 +9,11 @@ const CaseListing = ({ c, admin, deleteCase, }) => {
                     <div>
                         { c.name }
                         <div>
-                        <Button variant='danger' style={{ float: 'right' }} id='deleteCase' onClick={ () => deleteCase(c) }>Poista</Button>
-                        <Button variant='primary' style={{ float: 'right' }} id='editCase'>Muokkaa</Button>
+                            <Button variant='danger' style={{ float: 'right' }} id='deleteCase' onClick={ () => deleteCase(c) }>Poista</Button>
+                            <Button variant='primary' style={{ float: 'right' }} id='editCase'>Muokkaa</Button>
                         </div>
                     </div>
-                        :
+                    :
                     <></>
                 }
             </ListGroup.Item>
