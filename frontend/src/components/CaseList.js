1623543f3510c7734330e2d3be9bf44ca264f120
fannif <fanni.franssila@gmail.com>
2020-09-23 17:46:21 +0300

    small fixes

--- frontend/src/components/CaseList.js
+++ frontend/src/components/CaseList.js
@@ -6,8 +6,6 @@ const CaseList = () => {
     const cases = useSelector(state => state.case)?.sort((case1, case2) => case1.name.localeCompare(case2.name))
     const user = useSelector(state => state.user)
 
-    // Tänne ensimmäisten tyhjien tagien sisälle sitten
-    // Casea kuvaava komponentti, kun sellainen olemassa.
     return (
         <div>
             <h2>Tapaukset</h2>
--- frontend/src/components/CaseListing.js
+++ frontend/src/components/CaseListing.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Button, ListGroup } from 'react-bootstrap'
 
-const CaseListing = ({ case, admin }) => {
+const CaseListing = ({ case, admin, deleteCase, }) => {
     return (
         <ListGroup>
-            <ListGroup.Item key={ case.id }>
+            <ListGroup.Item key={ case.id } >
                 {admin ?
                     <div>
                         { case.name }
-                        <Button variant='danger' style={{ float: 'right' }} id='delete' onClick={() => deleteBact(bacterium)}>Poista</Button>
-                        <Button variant='primary' style={{ float: 'right' }} id='edit' onClick={() => setIsModified(true)}>Muokkaa</Button>
+                        <div>
+                        <Button variant='danger' style={{ float: 'right' }} id='deleteCase' onClick={ () => deleteCase(case) }>Poista</Button>
+                        <Button variant='primary' style={{ float: 'right' }} id='editCase'>Muokkaa</Button>
+                        </div>
                     </div>
                         :
                     <></>
