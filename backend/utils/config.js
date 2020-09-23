be75f1301416f9d085da3edefdc138cfac4f847b
millakortelainen <milla.kortelainen@helsinki.fi>
2020-09-23 11:30:24 +0300

    configurating gh actions

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const SECRET = process.env.SECRET
+console.log('totally not okay to print this stuff' + SECRET)
 
 let MONGODB_URI
 if (process.env.NODE_ENV === 'production') {
