683fe41f5579e4803b324cdd3ac9643026c114af
fannif <fanni.franssila@gmail.com>
2020-09-24 14:03:45 +0300

    edit style to match other pages better

--- frontend/src/components/CaseList.js
+++ frontend/src/components/CaseList.js
@@ -5,10 +5,11 @@ import CaseListing from './CaseListing'
 const CaseList = () => {
     const cases = useSelector(state => state.case)?.sort((case1, case2) => case1.name.localeCompare(case2.name))
     const user = useSelector(state => state.user)
+    const style = {margin: '10px', fontSize: '40px'}
 
     return (
         <div>
-            <h2>Tapaukset</h2>
+            <h2 style={style}>Tapaukset</h2>
             {cases ?
                 <ul>
                     {cases.map(c =>
