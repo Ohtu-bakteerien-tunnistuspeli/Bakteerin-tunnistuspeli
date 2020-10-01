da68394d8ad47051fb7b3612f572bf86bd21945d
Sini Suihkonen <sahsuihkonen@gmail.com>
2020-10-01 03:02:02 +0300

    Test Added Notification and new Test View

--- frontend/src/components/TestList.js
+++ frontend/src/components/TestList.js
@@ -1,42 +1,30 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import TestForm from './TestForm'
 import TestListing from './TestListing'
-import { deleteTest, updateTest } from '../reducers/testReducer'
 
 const TestList = () => {
+
     const style = {margin: '10px', fontSize: '40px'}
     const tests = useSelector(state => state.test)?.sort((test1, test2) => test1.name.localeCompare(test2.name))
     const user = useSelector(state => state.user)
-    const dispatch = useDispatch()
-    const deleteTests = test => {
-        dispatch(deleteTest(test, user.token))
-    }
-    const updateTests = (id, name, type, contImg, photoPos, photoNeg, bacteriaSpecificImg,  token) => {
-        dispatch(updateTest(id, name, type, contImg, photoPos, photoNeg, bacteriaSpecificImg, user.token))
-    }
 
-    return (
-        <div>
-            <h2 style={style}>Testit</h2>
-            <p></p>
+  return (
+      <div>
+        <h2 style={style}>Testit</h2>
+           <p></p>
             {user?.admin ?
-                <TestForm></TestForm>
+           <TestForm></TestForm>
                 :
-                <></>
+            <></>
             } 
             {tests ?
-                <ul>
-                    <p></p>
-                    {tests.map(test =>
-                        <TestListing key={test.id} test={test} deleteTest={deleteTests} updateTest={updateTests} isAdmin={user?.admin}></TestListing>
-                    )}
-                </ul>
-                :
-                <div>Ei testejä</div>
-                }
+            <TestListing></TestListing>
+                   :
+            <div>Ei testejä</div>
+            }
         </div>
-    )
+   )
 }
 
 export default TestList
--- frontend/src/components/TestListing.js
+++ frontend/src/components/TestListing.js
@@ -1,39 +1,70 @@
 import React, { useState } from 'react'
-import { ListGroup, Button } from 'react-bootstrap'
+import { Button, Table } from 'react-bootstrap'
 import ModalImage from './ModalImage'
 import TestEditForm from './TestEditForm'
+import { deleteTest, updateTest } from '../reducers/testReducer'
+import { useSelector, useDispatch } from 'react-redux'
 
-const TestListing = ({ test, deleteTest, isAdmin }) => {
+const TestListing = () => {
     const [isModified, setIsModified] = useState(false)
     const stopModify = () => {
-        setIsModified(false)
+    setIsModified(false)
     }
-    return (
-        <ListGroup>
-            <ListGroup.Item key={test.id}>
-                {test.name} {test.type} {test.positiveResultImage ? <ModalImage imageUrl={test.positiveResultImage.url} width={'10%'} height={'10%'}/>: <></>}
-                {test.negativeResultImage ? <ModalImage imageUrl={test.negativeResultImage.url} width={'10%'} height={'10%'}/> : <></>}
-                {test.controlImage ? <ModalImage imageUrl={test.controlImage.url} width={'10%'} height={'10%'}/> : <></>}
-                { console.log(test) }
-                {isModified ?
-                    <>
-                        <TestEditForm test={ test } stopModify={ stopModify } />
-                        <Button variant='secondary' id='stopEdit' style={{ float: 'right' }} onClick={stopModify}>Lopeta muokkaus</Button>
-                    </>
+    const tests = useSelector(state => state.test)?.sort((test1, test2) => test1.name.localeCompare(test2.name))
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    const deleteTests = test => {
+        dispatch(deleteTest(test, user.token))
+    }
+    const updateTests = (id, name, type, contImg, photoPos, photoNeg, bacteriaSpecificImg, token) => {
+        dispatch(updateTest(id, name, type, contImg, photoPos, photoNeg, bacteriaSpecificImg, user.token))
+    }
+
+return (
+    <div> 
+        <Table striped bordered hover>
+        <thead>
+        <tr>
+            <th>Nimi</th>
+            <th>Tyyppi</th>
+            <th>Kontrollikuva</th>
+            <th>Positiivinen oletus</th>
+            <th>Negatiivinen oletus</th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+       {tests.map(test =>
+            <tbody key={test.id}>
+                <tr key={test.id}>
+                <td>{test.name}</td> 
+                <td>{test.type}</td>
+                <td> {test.positiveResultImage ? <ModalImage imageUrl={test.positiveResultImage.url} width={'10%'} height={'10%'}/>: <></>} </td>
+                <td>{test.negativeResultImage ? <ModalImage imageUrl={test.negativeResultImage.url} width={'10%'} height={'10%'}/> : <></>} </td>
+                <td> {test.controlImage ? <ModalImage imageUrl={test.controlImage.url} width={'10%'} height={'10%'}/> : <></>} </td>
+            {/* console.log(test) */}
+            {isModified ?
+                <td>
+                    <TestEditForm test={ test } stopModify={ stopModify } />
+                    <Button variant='secondary' id='stopEdit' style={{ float: 'right' }} onClick={stopModify}>Lopeta muokkaus</Button>
+                </td>
                     :
-                    <>
-                        {isAdmin ?
-                            <>
-                                <Button variant='danger' style={{ float: 'right' }} id='delete' onClick={() => deleteTest(test)}>Poista</Button>
-                                <Button variant='primary' style={{ float: 'right' }} id='edit' onClick={() => setIsModified(true)}>Muokkaa</Button>
-                            </>
-                            :
-                            <></>
-                        }
-                    </>
+                <>
+            { user?.admin ?
+                <>
+                    <td> <Button variant='danger' style={{ float: 'right' }} id='delete' onClick={() => deleteTests(test)}>Poista</Button> </td>
+                    <td> <Button variant='primary' style={{ float: 'right' }} id='edit' onClick={() => setIsModified(true)}>Muokkaa</Button> </td>
+                </>
+                    :
+                <></>
+            }
+                </>
                 }
-            </ListGroup.Item>
-        </ListGroup>
+            </tr>
+        </tbody>
+        )}
+        </Table>
+    </div> 
     )
 }
 
