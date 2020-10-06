1c68f6c59b3ee61f4593d1717b6f7f72dabf3d91
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2020-10-06 15:07:00 +0300

    Add listing tests in-game
    Co-Authored-By: Jeeses313 <46317768+Jeeses313@users.noreply.github.com>
    Co-Authored-By: Timo Järvenpää <32742872+TimoJarvenpaa@users.noreply.github.com>

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -9,9 +9,19 @@ const GamePage = () => {
     const game = useSelector(state => state.game)
     const user = useSelector(state => state.user)
     const [selectedSamples, setSelectedSamples] = useState([])
-    /* Testien erottelu tyypin perusteella
-    const tests = useSelector(state=>state.tests)
+    const tests = useSelector(state => state.test)
+    /*
+    const types = tests.map(test => test.type)
+    types.reduce(function(a,b){
+        if(a.indexOf(b)<0)a.push(b)
+        return a
+    },[])
     */
+    const testsToShow = tests.filter(test => test.type === 'Testi')
+    const cultivationsToShow = tests.filter(test => test.type === 'Viljely')
+    const stainingsToShow = tests.filter(test => test.type === 'Värjäys')
+    const othersToShow = tests.filter(test => test.type !== 'Testi' && test.type !== 'Viljely' && test.type !== 'Värjäys')
+
     const sampleCheckBoxChange = (description) => {
         if (selectedSamples.includes(description)) {
             //console.log(selectedSamples.filter(sample => sample !== description))
@@ -21,9 +31,15 @@ const GamePage = () => {
             setSelectedSamples([...selectedSamples, description])
         }
     }
+
     const sampleSubmit = (event) => {
         event.preventDefault()
         dispatch(checkSamples(game, { samples: selectedSamples }, user.token))
+    }
+
+    const handleTest = (testId) => {
+        console.log(testId)
+        console.log([...game.correctTests, testId])
     }
 
     return (
@@ -53,6 +69,52 @@ const GamePage = () => {
                                     :
                                     <>
                                         <h1>Laboratoriotutkimukset</h1>
+                                        <div style={{ backgroundColor: "#F5F5F5" }}>
+                                            {
+                                                (cultivationsToShow && cultivationsToShow.length > 0) ?
+                                                    <>
+                                                        <h2>Viljelyt</h2>
+                                                        {cultivationsToShow.map(test =>
+                                                            <Button key={test.id} variant='warning' onClick={() => handleTest(test.id)}>{test.name}{game.correctTests.includes(test.id) ? <i className="fas fa-check"></i> : <></>}</Button>
+                                                        )}
+                                                    </>
+                                                    :
+                                                    <></>
+                                            }
+                                            {
+                                                (testsToShow && testsToShow.length > 0) ?
+                                                    <>
+                                                        <h2>Testit</h2>
+                                                        {testsToShow.map(test =>
+                                                            <Button key={test.id} variant='info' onClick={() => handleTest(test.id)}>{test.name}{game.correctTests.includes(test.id) ? <i className="fas fa-check"></i> : <></>}</Button>
+                                                        )}
+                                                    </>
+                                                    :
+                                                    <></>
+                                            }
+                                            {
+                                                (stainingsToShow && stainingsToShow.length > 0) ?
+                                                    <>
+                                                        <h2>Värjäys</h2>
+                                                        {stainingsToShow.map(test =>
+                                                            <Button key={test.id} variant='success' onClick={() => handleTest(test.id)}>{test.name}{game.correctTests.includes(test.id) ? <i className="fas fa-check"></i> : <></>}</Button>
+                                                        )}
+                                                    </>
+                                                    :
+                                                    <></>
+                                            }
+                                            {
+                                                (othersToShow && othersToShow.length > 0) ?
+                                                    <>
+                                                        <h2>Muut</h2>
+                                                        {othersToShow.map(test =>
+                                                            <Button key={test.id} variant='secondary' onClick={() => handleTest(test.id)}>{test.name}{game.correctTests.includes(test.id) ? <i className="fas fa-check"></i> : <></>}</Button>
+                                                        )}
+                                                    </>
+                                                    :
+                                                    <></>
+                                            }
+                                        </div>
                                     </>
                             }
                         </Tab>
