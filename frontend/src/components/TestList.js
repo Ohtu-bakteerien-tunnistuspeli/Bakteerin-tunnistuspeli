0a817a9379ca908106f8034e8161980d1e34a44c
Sini Suihkonen <sahsuihkonen@gmail.com>
2020-10-13 16:34:38 +0300

    Test List fixed

--- frontend/src/components/TestList.js
+++ frontend/src/components/TestList.js
@@ -2,29 +2,46 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import TestForm from './TestForm'
 import TestListing from './TestListing'
+import { Table } from 'react-bootstrap'
 
 const TestList = () => {
 
-    const style = {margin: '10px', fontSize: '40px'}
-    const tests = useSelector(state => state.test)?.sort((test1, test2) => test1.name.localeCompare(test2.name))
-    const user = useSelector(state => state.user)
+const style = {margin: '10px', fontSize: '40px'}
+const tests = useSelector(state => state.test)?.sort((test1, test2) => test1.name.localeCompare(test2.name))
+const user = useSelector(state => state.user)
 
-  return (
-      <div>
-        <h2 style={style}>Testit</h2>
-           <p></p>
-            {user?.admin ?
-           <TestForm></TestForm>
-                :
-            <></>
-            } 
-            {tests ?
-            <TestListing></TestListing>
-                   :
-            <div>Ei testejä</div>
-            }
-        </div>
-   )
+return (
+  <div>
+      <h2 style={style}>Testit</h2>
+      <p></p>
+      {user?.admin ?
+      <TestForm></TestForm>
+    :
+      <></>
+    } 
+      {tests ?
+        <Table striped bordered hover>
+        <thead>
+            <tr>
+            <th>Nimi</th>
+            <th>Tyyppi</th>
+            <th>Kontrollikuva</th>
+            <th>Positiivinen oletus</th>
+            <th>Negatiivinen oletus</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {tests.map(test => 
+            <TestListing key={test.id} test={test}></TestListing>
+          )}
+        </tbody>
+        </Table>
+        :
+        <div>Ei Testejä</div> 
+      }
+    </div>
+  )
 }
 
 export default TestList
--- frontend/src/components/TestListing.js
+++ frontend/src/components/TestListing.js
@@ -1,71 +1,43 @@
 import React, { useState } from 'react'
-import { Button, Table } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import ModalImage from './ModalImage'
 import TestEditForm from './TestEditForm'
-import { deleteTest, updateTest } from '../reducers/testReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-const TestListing = () => {
-    const [isModified, setIsModified] = useState(false)
-    const stopModify = () => {
-    setIsModified(false)
-    }
-    const tests = useSelector(state => state.test)?.sort((test1, test2) => test1.name.localeCompare(test2.name))
-    const user = useSelector(state => state.user)
-    const dispatch = useDispatch()
-    const deleteTests = test => {
-        dispatch(deleteTest(test, user.token))
-    }
-    const updateTests = (id, name, type, contImg, photoPos, photoNeg, bacteriaSpecificImg, token) => {
-        dispatch(updateTest(id, name, type, contImg, photoPos, photoNeg, bacteriaSpecificImg, user.token))
-    }
+const TestListing = ( {test } ) => {
+const [isModified, setIsModified] = useState(false)
+const stopModify = () => {
+setIsModified(false)
+}
+
+const user = useSelector(state => state.user)
+const bacteria = useSelector(state => state.bacteria)?.sort((bacterium1, bacterium2) => bacterium1.name.localeCompare(bacterium2.name))
 
 return (
-    <div> 
-        <Table striped bordered hover>
-        <thead>
-        <tr>
-            <th>Nimi</th>
-            <th>Tyyppi</th>
-            <th>Kontrollikuva</th>
-            <th>Positiivinen oletus</th>
-            <th>Negatiivinen oletus</th>
-            <th></th>
-            <th></th>
-        </tr>
-    </thead>
-       {tests.map(test =>
-            <tbody key={test.id}>
-                <tr key={test.id}>
-                <td>{test.name}</td> 
-                <td>{test.type}</td>
-                <td> {test.positiveResultImage ? <ModalImage imageUrl={test.positiveResultImage.url} width={'10%'} height={'10%'}/>: <></>} </td>
-                <td>{test.negativeResultImage ? <ModalImage imageUrl={test.negativeResultImage.url} width={'10%'} height={'10%'}/> : <></>} </td>
-                <td> {test.controlImage ? <ModalImage imageUrl={test.controlImage.url} width={'10%'} height={'10%'}/> : <></>} </td>
-            {/* console.log(test) */}
-            {isModified ?
-                <td>
-                    <TestEditForm test={ test } stopModify={ stopModify } />
-                    <Button variant='secondary' id='stopEdit' style={{ float: 'right' }} onClick={stopModify}>Lopeta muokkaus</Button>
-                </td>
-                    :
-                <>
-            { user?.admin ?
-                <>
-                    <td> <Button variant='danger' style={{ float: 'right' }} id='delete' onClick={() => deleteTests(test)}>Poista</Button> </td>
-                    <td> <Button variant='primary' style={{ float: 'right' }} id='edit' onClick={() => setIsModified(true)}>Muokkaa</Button> </td>
-                </>
-                    :
-                <></>
-            }
-                </>
-                }
-            </tr>
-        </tbody>
-        )}
-        </Table>
-    </div> 
-    )
+<tr key={test.id}>
+<td>{test.name}</td> 
+<td>{test.type}</td>
+<td> {test.positiveResultImage ? <ModalImage imageUrl={test.positiveResultImage.url} width={'10%'} height={'10%'}/>: <></>} </td>
+<td>{test.negativeResultImage ? <ModalImage imageUrl={test.negativeResultImage.url} width={'10%'} height={'10%'}/> : <></>} </td>
+<td> {test.controlImage ? <ModalImage imageUrl={test.controlImage.url} width={'10%'} height={'10%'}/> : <></>} </td>
+{isModified ?
+<td>
+<TestEditForm test={ test } stopModify={ stopModify } bacteria={bacteria} />
+<Button variant='secondary' id='stopEdit' style={{ float: 'right' }} onClick={stopModify}>Lopeta muokkaus</Button>
+</td>
+:
+<>
+{ user?.admin ?
+<>
+<td> <Button variant='primary' style={{ float: 'right' }} id='edit' onClick={() => setIsModified(true)}>Muokkaa</Button> </td>
+</>
+:
+<></>
+}
+</>
+}
+</tr>
+)
 }
 
 export default TestListing
