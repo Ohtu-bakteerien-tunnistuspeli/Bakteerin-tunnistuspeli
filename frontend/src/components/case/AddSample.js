819b32a9a8b277f4934f9ee9c207a0d0ecd1302c
Jeeses313 <jesse.vilkman@hotmail.fi>
2020-10-22 10:22:16 +0300

    Fix cypress tests for ga

--- frontend/src/components/case/AddSample.js
+++ frontend/src/components/case/AddSample.js
@@ -3,20 +3,20 @@ import { Button, Form } from 'react-bootstrap'
 
 
 const AddSample = ({ sample, setSample, addSample }) => {
-    return (<Form.Group>
-        <Form.Control
-            controlId='samples'
-            value={sample.description}
-            onChange={(event) => setSample({ ...sample, description: event.target.value })}
-        />
-        <Form.Check
-            type='checkbox'
-            id='isRightAnswer'
-            label='Oikea vastaus'
-            checked={sample.rightAnswer}
-            onChange={() => setSample({ ...sample, rightAnswer: !sample.rightAnswer })} />
-        <Button type='button' id='addSample' onClick={() => addSample(sample.description, sample.rightAnswer)}>+</Button>
-    </Form.Group>
+    return (
+        <Form.Group controlId='samples'>
+            <Form.Control
+                value={sample.description}
+                onChange={(event) => setSample({ ...sample, description: event.target.value })}
+            />
+            <Form.Check
+                type='checkbox'
+                id='isRightAnswer'
+                label='Oikea vastaus'
+                checked={sample.rightAnswer}
+                onChange={() => setSample({ ...sample, rightAnswer: !sample.rightAnswer })} />
+            <Button type='button' id='addSample' onClick={() => addSample(sample.description, sample.rightAnswer)}>+</Button>
+        </Form.Group>
     )
 }
 
