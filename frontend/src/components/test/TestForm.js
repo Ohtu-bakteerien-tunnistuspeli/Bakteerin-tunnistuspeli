339c494a3bf6c81ba978a63c12467b084d027452
outisa <43496587+outisa@users.noreply.github.com>
2020-11-10 11:55:32 +0200

    Change error message for name

--- frontend/src/components/test/TestForm.js
+++ frontend/src/components/test/TestForm.js
@@ -113,8 +113,8 @@ const TestForm = ({ testToEdit }) => {
     /* schema for validation */
     const TestSchema = Yup.object().shape({
         testName: Yup.string()
-            .min(2, 'Nimi on liian lyhyt.')
-            .max(50, 'Nimi on liian pitkä')
+            .min(2, 'Testin nimen tulee olla vähintään 2 merkkiä pitkä.')
+            .max(100, 'Testin nimen tulee olla enintään 100 merkkiä pitkä.')
             .required('Pakollinen kenttä.')
             .test('unique', 'Nimen tulee olla uniikki', function (name) {
                 if (testToEdit) {
