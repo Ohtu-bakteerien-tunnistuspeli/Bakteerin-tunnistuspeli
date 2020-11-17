f1d5fc741bd2e41492832bd0964d8b528e441810
Jeeses313 <jesse.vilkman@hotmail.fi>
2020-11-17 14:08:46 +0200

    Add warning that deleting user deletes credits

--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -30,6 +30,7 @@ const ProfilePage = () => {
                 modalHeader={`Käyttäjän ${user.username} poistamisen varmennus`}
                 warningText='Olet poistamassa käyttäjätunnustasi.
                 Et voi palauttaa käyttäjätunnustasi, kun olet sen poistanut.
+                Käyttäjätunnuksen poistaminen poistaa myös suoritukset.
                 Jos olet varma, että haluat poistaa käyttäjätunnksesi, niin
                 kirjoita käyttäjänimesi ja paina nappia poistaaksesi profiilisi.'
                 functionToExecute={userDelete}
