47a73ac0a43e11c0c03b7c30958822051c73f4ca
Jeeses313 <jesse.vilkman@hotmail.fi>
2020-11-24 14:50:19 +0200

    Add setUser dispatch to userReducer

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -21,6 +21,9 @@ const reducer = (state = null, action) => {
     case 'REGISTER': {
         return action.data
     }
+    case 'SET_USER': {
+        return action.data
+    }
     default: return state
     }
 }
@@ -137,6 +140,16 @@ export const generateSingleUsePassword = (username, email, history) => {
             dispatch(setNotification({ message: response.message, success: true, show: true }))
             history.push('/')
         }
+    }
+}
+
+export const setUser = (user) => {
+    return async dispatch => {
+        window.localStorage.setItem('loggedUser', JSON.stringify(user))
+        dispatch({
+            type: 'SET_USER',
+            data: user
+        })
     }
 }
 
