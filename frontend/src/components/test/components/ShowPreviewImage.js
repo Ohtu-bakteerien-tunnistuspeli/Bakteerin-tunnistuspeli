cb05f9193ecb1d632c07ebbedb9f36b8620c2be2
fannif <fanni.franssila@gmail.com>
2020-11-25 13:33:47 +0200

    Show preview of completion image in caseform

--- frontend/src/components/test/components/ShowPreviewImage.js
+++ frontend/src/components/test/components/ShowPreviewImage.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Image } from 'react-bootstrap'
 
-const ShowPreviewImage = ({ imgPreview }) =>
-    imgPreview ?
-        <Image src={imgPreview} thumbnail width={100}></Image>
-        : null
+const ShowPreviewImage = ({ imgPreview }) => {
+    console.log(imgPreview)
+    return (
+        imgPreview ?
+            <Image src={imgPreview} thumbnail width={100}></Image>
+            : null
+    )
+}
 
 export default ShowPreviewImage
